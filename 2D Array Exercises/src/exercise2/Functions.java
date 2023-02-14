c5777aaf9e1ae98785f55b74e3e03a1531169c5c
aleleotta <alessandro.leotta@iesnervion.es>
2023-02-14 10:58:09 +0100

    .

--- 2D Array Exercises/src/exercise2/Functions.java
+++ 2D Array Exercises/src/exercise2/Functions.java
@@ -2,3 +2,8 @@ package exercise2;
 
 public class Functions {
 }
+/*
+ * Crea un programa que cree una matriz de 10x10 e 
+ * introduzca los valores de las tablas de multiplicar del 1 al 10
+ * (cada tabla de multiplicar en una fila). Luego mostrará la matriz por pantalla.
+*/
--- 2D Array Exercises/src/exercise2/Main.java
+++ 2D Array Exercises/src/exercise2/Main.java
@@ -8,3 +8,8 @@ public class Main {
 		sc.close();
 	}
 }
+/*
+ * Crea un programa que cree una matriz de 10x10 e 
+ * introduzca los valores de las tablas de multiplicar del 1 al 10
+ * (cada tabla de multiplicar en una fila). Luego mostrará la matriz por pantalla.
+*/
